49561d626f6642cc8ff6bd1997cf6e7df89beef0
WebAppAI0410 <webapptest0410@gmail.com>
2025-05-21 04:52:40 +0900

    refactored9

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
 import type { NextConfig } from "next";
+import path from 'path';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  webpack: (config, { isServer }) => {
+    // functions ディレクトリを完全に除外
+    config.externals = [
+      ...(config.externals || []),
+      // Deno を外部として扱う
+      { 'deno': 'Deno' },
+    ];
+
+    // functions ディレクトリを exclude
+    config.module.rules.push({
+      test: /functions\//,
+      exclude: /node_modules/,
+      use: 'ignore-loader',
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
